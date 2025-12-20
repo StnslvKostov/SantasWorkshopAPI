886e3a256c914e551a973d4908f204d31b12268a
Stanislav <kostovstnslv@gmail.com>
2025-12-20 11:04:19 +0200

    Created SantaWorkshopAPI with spring initializr.

---
+++ demo/src/main/java/com/swapi/exam/SantaWorkshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.swapi.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaWorkshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaWorkshopApiApplication.class, args);
+	}
+
+}
